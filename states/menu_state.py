02b3e9daa562310fd1c20a754169d41a6e58609a
Amin Tabti <amin.tabti1811@gmail.com>
2026-03-19 23:00:39 +0100

    fikset feil + startet på main menu

--- states/menu_state.py
+++ states/menu_state.py
@@ -8,6 +8,9 @@ import pygame
 class MenuState(BaseState):
     def __init__(self):
         super().__init__()
+        self.bakrund_load =  pygame.image.load("Gamejam_Amin_Leo/assets/main_menu_bilde.png").convert()
+        self.bakrund = pygame.transform.scale(self.bakrund_load, (1200, 600))
+        self.start_box = pygame.Rect(0, 50, 590, 300) #x, y, bredde 590, høyde
 
     def handle_events(self, events : list[pygame.event.Event]):
         for event in events:
@@ -24,5 +27,6 @@ class MenuState(BaseState):
         pass
 
     def draw(self, surface: pygame.Surface):
-        surface.fill((255, 0, 0))
+        surface.blit(self.bakrund, (0,0))
+        pygame.draw.rect(surface, (255, 0, 0), self.start_box)
         self.draw_text(surface, "Du er i hovedmenyen! Trykk SPACE for å starte.", self.font, (255, 255, 255), (600, 300))
