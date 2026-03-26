be55a8d54da09f9ba113b9a292ce0a1b5ccef3db
Amin Tabti <amin.tabti1811@gmail.com>
2026-03-26 12:12:29 +0100

    fiksa en error - måtte kopiere hele koden inn

--- states/game_state.py
+++ states/game_state.py
@@ -9,10 +9,10 @@ from random import randint
 class GameState(BaseState):
     def __init__(self):
         super().__init__()
-        self.bakrund_load =  pygame.image.load("assets/battlefield.png").convert()
-        self.bakrund = pygame.transform.scale(self.bakrund_load, (1200, 600))
-        self.spill_bane1 = pygame.Rect(225, 550, 750, 50) #x, y, bredde, høyde
-        self.spill_bane2 = pygame.Rect(95, 450, 1000, 100)
+        self.bakrund_load =  pygame.image.load("assets/battlefield2.png").convert()
+        self.bakrund = pygame.transform.scale(self.bakrund_load, (1300, 700))
+        self.spill_bane2 = pygame.Rect(215, 445, 873, 85) #x, y, bredde, høyde
+        self.spill_bane1 = pygame.Rect(275, 530, 775, 50) # 190, 480, 920, 20 -- ikke slett dette!
        
     #----------Chat under ---------------------
     def startup(self, persistent):
@@ -27,10 +27,10 @@ class GameState(BaseState):
             self.høyde = 100
             bilde1 = "assets/luigi_karakter.png"
         self.player1 = Player(300, 0, self, kontroller={
-        "left": pygame.K_a, "right": pygame.K_d, "up": pygame.K_w, "down": pygame.K_s}, bilde=bilde1, hoppe_bilde="assets/Birk_hopp.png")
+        "left": pygame.K_a, "right": pygame.K_d, "up": pygame.K_w, "down": pygame.K_s}, bilde=bilde1)
 
         self.player2 = Player(800, 0, self, kontroller={
-        "left": pygame.K_LEFT, "right": pygame.K_RIGHT, "up": pygame.K_UP, "down": pygame.K_DOWN}, bilde = "assets/luigi_karakter.png", hoppe_bilde="assets/Birk_hopp.png" )
+        "left": pygame.K_LEFT, "right": pygame.K_RIGHT, "up": pygame.K_UP, "down": pygame.K_DOWN}, bilde = "assets/luigi_karakter.png")
         
     #----------Chat over------------------------
 
@@ -61,8 +61,8 @@ class GameState(BaseState):
 
     def draw(self, surface: pygame.Surface):
         surface.blit(self.bakrund, (0,0))
-        #pygame.draw.rect(surface, (255, 0, 0), self.spill_bane1, 2)
-        #pygame.draw.rect(surface, (255, 0, 0), self.spill_bane2, 2)
+        pygame.draw.rect(surface, (255, 0, 0), self.spill_bane1, 2)
+        pygame.draw.rect(surface, (255, 0, 0), self.spill_bane2, 2)
         self.player1.draw(surface)
         self.player2.draw(surface)
 
@@ -83,10 +83,12 @@ class Player(GameObject):
     def __init__(self, x, y, game, kontroller, bilde):
         self.game = game
         super().__init__(x, y, self.game.bredde, self.game.høyde)
-        self.speed = 9
+        self.speed = 7
         self.vy = 0
-        self.jumping = False
         self.på_bakken = False
+
+        self.antall_hopp = 0
+        self.max_hopp = 2
         
         self.kontroller = kontroller
         self.bilde = pygame.image.load(bilde)
@@ -100,37 +102,18 @@ class Player(GameObject):
         gammel_bottom = self.rect.bottom  # brukte chat for å finne ut av hvordan man gjøre at kollisjonene funker å sidene av platformene også
         
         keys = pygame.key.get_pressed()
-        #---------Chat under ---------
-        if keys[self.kontroller["left"]]:
-            self.rect.x -= self.speed
-        if keys[self.kontroller["right"]]:
-            self.rect.x += self.speed
-        if keys[self.kontroller["up"]] and self.på_bakken:
-            self.vy = -10  
-            self.på_bakken = False
-        if keys[self.kontroller["down"]]:
-            self.rect.y += self.speed
-        #-------- Chat over ---------
-        if self.jumping == True and self.på_bakken == True:
-            self.vy = -8
-            self.jumping = False
-            if self.valgtbirk:
-                self.bilde = self.hoppe_bilde
-        self.vy += 0.181
-        self.rect.y += self.vy         
-        
-        self.på_bakken = False
+        if keys[self.kontroller["left"]]: # chat
+            self.rect.x -= self.speed #chat
+        if keys[self.kontroller["right"]]: #chat
+            self.rect.x += self.speed #chat
 
-        if self.rect.colliderect(self.game.spill_bane1) and gammel_bottom <=self.game.spill_bane1.top :
-            self.rect.bottom = self.game.spill_bane1.top
-            self.vy = 0
-            self.på_bakken = True
-            self.bilde = self.vanlig_bilde
-        if self.rect.colliderect(self.game.spill_bane2) and gammel_bottom <=self.game.spill_bane2.top  :
-            self.rect.bottom = self.game.spill_bane2.top
-            self.vy = 0   
-            self.på_bakken = True
-=======
+        if keys[self.kontroller["up"]]:
+            if self.på_bakken or self.antall_hopp < self.max_hopp:
+                self.vy = -8  
+                self.på_bakken = False
+                self.antall_hopp += 1
+
+        if keys[self.kontroller["down"]]:
             if self.vy <= 0:
                 self.vy = 0
             else:
@@ -158,4 +141,3 @@ class Player(GameObject):
 
                 elif self.rect.left < platform.right and self.rect.right > platform.right:
                     self.rect.left = platform.right
->>>>>>> 8bbd2a159ef0ee0950333e99fbf7d97a0436bf73
